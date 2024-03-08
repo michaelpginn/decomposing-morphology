ad8197255d8e1012203f8569a6a8c3066989b240
michaelpginn <milespergallon271@gmail.com>
2024-03-07 19:06:14 -0700

    Create training script

---
+++ scripts/train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1           # Number of requested nodes
+#SBATCH --gres=gpu
+#SBATCH --ntasks=4          # Number of requested cores
+#SBATCH --mem=32G
+#SBATCH --time=7-00:00:00          # Max walltime              # Specify QOS
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --out=train.%j.out      # Output file name
+#SBATCH --error=train.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=michael.ginn@colorado.edu
+
+# purge all existing modules
+module purge
+# Load the python module
+module load anaconda
+# Run Python Script
+conda activate AutoIGT
+cd "/projects/migi8081/decomposing-morphology/src"
+
+python3 
